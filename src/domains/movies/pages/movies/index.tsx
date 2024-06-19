ff0168c38f99764f23cb0e0b4351eee2282a83e0
Alexander Musienko <alexmvid0@gmail.com>
2024-06-19 19:35:37 +0300

    added skeleton for movies page (movies list)

--- src/domains/movies/pages/movies/index.tsx
+++ src/domains/movies/pages/movies/index.tsx
@@ -2,17 +2,27 @@ import { useSearchParams } from "react-router-dom";
 import { MOVIES_LIST_MOCK } from "../../mock";
 import { MovieItem } from "./components/MovieItem";
 import { Grid } from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { LoadingMovies } from "./components/LoadingMovies";
+import { useStores } from "@/common/hooks/use-stores";
 
-export const Movies = () => {
+export const Movies = observer(() => {
+  const {
+    moviesStore: { isLoading },
+  } = useStores();
   const [searchParams] = useSearchParams();
   console.log(searchParams.get("filter"));
   return (
     <Grid container alignItems="stretch" spacing={2}>
-      {MOVIES_LIST_MOCK.docs.map((movie) => (
-        <Grid item xs={12} sm={4} lg={3}>
-          <MovieItem key={movie.id} {...movie} />
-        </Grid>
-      ))}
+      {isLoading ? (
+        <LoadingMovies />
+      ) : (
+        MOVIES_LIST_MOCK.docs.map((movie) => (
+          <Grid item xs={12} sm={4} lg={3}>
+            <MovieItem key={movie.id} {...movie} />
+          </Grid>
+        ))
+      )}
     </Grid>
   );
-};
+});
